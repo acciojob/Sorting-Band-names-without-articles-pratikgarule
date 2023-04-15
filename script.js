ae09f5a90727489386a68a29cc8550edbe52ecee
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-04-15 15:28:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+
+touristSpots = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
+
+const sortedBands = bands.sort(function (a, b){  
+  if(strip(a) > strip(b) {
+    return 1
+  } else {
+    return -1
+  }
+})
+document.querySelector('#bands').innerHTML =  
+  sortedBands
+    .map(band => `
